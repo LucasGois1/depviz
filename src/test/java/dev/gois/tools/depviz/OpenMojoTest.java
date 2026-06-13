c3241a4cd58766ded29c6011a7f6a4ed792e11e0
Lucas Gois <lucassampaiodegois1@gmail.com>
2026-06-13 15:31:30 -0300

    fix: validate generated plugin descriptor

--- src/test/java/dev/gois/tools/depviz/OpenMojoTest.java
+++ src/test/java/dev/gois/tools/depviz/OpenMojoTest.java
@@ -2,29 +2,48 @@ package dev.gois.tools.depviz;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.nio.charset.StandardCharsets;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 import org.apache.maven.plugin.AbstractMojo;
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
 class OpenMojoTest {
     @Test
-    void isMavenMojoNamedOpen() throws Exception {
+    void pluginDescriptorDefinesOpenMojo() throws Exception {
         assertThat(AbstractMojo.class.isAssignableFrom(OpenMojo.class)).isTrue();
-        String classBytes = readClassBytes(OpenMojo.class);
-        assertThat(classBytes).contains("Lorg/apache/maven/plugins/annotations/Mojo;");
-        assertThat(classBytes).contains("open");
+
+        Document descriptor = readPluginDescriptor();
+        assertThat(text(descriptor, "/plugin/requiredMavenVersion")).isEqualTo("3.9.0");
+        Node openMojo = node(descriptor, "/plugin/mojos/mojo[goal='open']");
+        assertThat(openMojo).isNotNull();
+        assertThat(text(openMojo, "goal")).isEqualTo("open");
+        assertThat(text(openMojo, "requiresProject")).isEqualTo("true");
+        assertThat(text(openMojo, "threadSafe")).isEqualTo("true");
+
         Method execute = OpenMojo.class.getMethod("execute");
         assertThat(execute.getReturnType()).isEqualTo(Void.TYPE);
     }
 
-    private static String readClassBytes(Class<?> type) throws IOException {
-        String resourceName = type.getSimpleName() + ".class";
-        try (InputStream inputStream = type.getResourceAsStream(resourceName)) {
+    private static Document readPluginDescriptor() throws Exception {
+        try (InputStream inputStream = OpenMojoTest.class.getClassLoader()
+                .getResourceAsStream("META-INF/maven/plugin.xml")) {
             assertThat(inputStream).isNotNull();
-            return new String(inputStream.readAllBytes(), StandardCharsets.ISO_8859_1);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            return factory.newDocumentBuilder().parse(inputStream);
         }
+    }
+
+    private static Node node(Object item, String expression) throws Exception {
+        return (Node) XPathFactory.newInstance().newXPath().evaluate(expression, item, XPathConstants.NODE);
+    }
+
+    private static String text(Object item, String expression) throws Exception {
+        return XPathFactory.newInstance().newXPath().evaluate(expression, item);
     }
 }
